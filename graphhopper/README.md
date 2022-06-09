c83603cd9d2ac4cabf8246587ef7480b015960ac
OngTu <levantu01111999@gmail.com>
2022-06-09 13:35:04 +0700

    update gitignore

---
+++ graphhopper/README.md
@@ -0,0 +1,2 @@
+# run graphhopper
+java -Ddw.graphhopper.datareader.file=map1.osm -jar graphhopper-web-5.1.jar server config-example.yml
---
+++ web/app1/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+# Create your models here.
+class Icons(models.Model):
+    name = models.CharField(max_length=50, null=False, blank=False)
+    code = models.IntegerField(null=False, blank=False)
+    image = models.ImageField(upload_to='images/', null=False, blank=False)
+    def __str__(self):
+        return str(self.code) + "." + self.name
+
+    class Meta:
+        db_table = 'icons'
+
+    
