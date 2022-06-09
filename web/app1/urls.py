976d42cd98736f427c93725a68f313323a993fd3
OngTu <levantu01111999@gmail.com>
2022-06-09 13:32:43 +0700

    update gitihnore

--- web/app1/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.IndexClass.as_view(), name='index'),
-    path('icon/<str:code>', views.GetIcon, name='icons'),
-]
